8c89cf7236507300c8be2a82975d9f41e7795e21
mukeshpatil1 <mukesh.patil@citruspay.com>
2017-01-18 17:47:31 +0530

    PlugNPlay SDK Released Version 1.0

---
+++ SwiftExample/plugnplay-sdk/cocoapods/Example/Pods/Target Support Files/PlugNPlay/PlugNPlay-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double PlugNPlayVersionNumber;
+FOUNDATION_EXPORT const unsigned char PlugNPlayVersionString[];
+
---
+++ SwiftExample/plugnplay-sdk/cocoapods/Example/Pods/Target Support Files/Pods-PlugNPlay_Tests/Pods-PlugNPlay_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlugNPlay_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PlugNPlay_Tests
+@end
