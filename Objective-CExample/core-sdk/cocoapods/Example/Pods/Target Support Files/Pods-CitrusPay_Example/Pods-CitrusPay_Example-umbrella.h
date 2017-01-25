891e31e6b5971dcd19d26531a375caa4fa2ca161
mukeshpatil1 <mukesh.patil@citruspay.com>
2017-01-25 18:50:19 +0530

    CoreSDK - Ignore Pods from Cocoapods Example

--- Objective-CExample/core-sdk/cocoapods/Example/Pods/Target Support Files/Pods-CitrusPay_Example/Pods-CitrusPay_Example-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_CitrusPay_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CitrusPay_ExampleVersionString[];
-
